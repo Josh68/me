0549eeb0a3cdfd527cd0ffefc99c22ba008b89e0
Josh Schneider <josh68@gmail.com>
2020-02-13 06:49:20 -0800

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/me/app-65d0d082bab272558536.js","0377aa8951fdc41bc324838ceb9e7c56"],["/me/app-abbfc3a440bd468ed0b3.js","2d9f117a9c4021c283e103f4d67637d7"],["/me/app-ef1ba89c70794109d3c8.js","13976290fe8ff1a636c9a4d6228f61db"],["/me/commons-14096613a231fa9be45b.js","d71fbe274738991df0a8fc0723dbc7d8"],["/me/index.html","e3054e5a4977513d089f734dcdeff5e3"],["/me/offline-plugin-app-shell-fallback/index.html","1c62b50215ed2c2371a1fca9d2e4f245"]];
+var precacheConfig = [["/me/app-44fcccd9164a2c8b435c.js","f35d4ab4da210f699f69b8ac87f5115f"],["/me/app-65d0d082bab272558536.js","0377aa8951fdc41bc324838ceb9e7c56"],["/me/app-abbfc3a440bd468ed0b3.js","2d9f117a9c4021c283e103f4d67637d7"],["/me/app-ef1ba89c70794109d3c8.js","13976290fe8ff1a636c9a4d6228f61db"],["/me/commons-14096613a231fa9be45b.js","d71fbe274738991df0a8fc0723dbc7d8"],["/me/index.html","a37b0396630b5cb75e8718e814b8995b"],["/me/offline-plugin-app-shell-fallback/index.html","e0424c11d7ce98128602aba4a309b773"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
